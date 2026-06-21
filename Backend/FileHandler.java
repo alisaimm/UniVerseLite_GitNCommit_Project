7f2540654d352cfd6dc6beb6bd1c76735a3ed5c0
adityaraut649 <vraut3468@gmail.com>
2026-06-21 19:09:00 +0530

    Remove unused serialVersionUID from FileHandler

--- Backend/FileHandler.java
+++ Backend/FileHandler.java
@@ -1,38 +1,44 @@
 package Backend;
+
 import java.io.*;
 import java.util.*;
 
 public class FileHandler {
-private static final long serialVersionUID = 1L;
-    //1) Write to File
-    public void saveToFile(CampusData data, String filename)
-    {
+
+    // 1) Write to File
+    public void saveToFile(CampusData data, String filename) {
         File file = new File(filename);
-        try{
+        try {
             FileOutputStream fos = new FileOutputStream(file);
             ObjectOutputStream oos = new ObjectOutputStream(fos);
             oos.writeObject(data);
             oos.close();
+        } catch (FileNotFoundException e) {
+            System.err.println("File Not Found.");
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
         }
-        catch(FileNotFoundException e){ System.err.println("File Not Found."); e.printStackTrace();}
-        catch(IOException e){ e.printStackTrace();}
     }
-    //2) Read from File
-    public CampusData loadOnStartup(String filename)
-    {
-        try{
+
+    // 2) Read from File
+    public CampusData loadOnStartup(String filename) {
+        try {
             File file = new File(filename);
             FileInputStream fis = new FileInputStream(file);
             ObjectInputStream ois = new ObjectInputStream(fis);
-            CampusData data = (CampusData)ois.readObject();
+            CampusData data = (CampusData) ois.readObject();
             ois.close();
             return data;
+        } catch (FileNotFoundException e) {
+            System.err.println("File Not Found.");
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        } catch (ClassNotFoundException e) {
+            e.printStackTrace();
         }
-        catch(FileNotFoundException e){ System.err.println("File Not Found."); e.printStackTrace();}
-        catch(IOException e){ e.printStackTrace();}
-        catch(ClassNotFoundException e){ e.printStackTrace();}
-        //In case of error
+        // In case of error
         return new CampusData();
     }
-    }
-
+}
